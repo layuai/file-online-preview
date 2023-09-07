e9c24836cee75ce225078c74877094f8b79f6d37
gaoxiongzaq <admin@cxcp.com>
2023-09-07 09:52:44 +0800

    新增缓存标记,防止重复转换

--- server/src/main/java/cn/keking/service/impl/CompressFilePreviewImpl.java
+++ server/src/main/java/cn/keking/service/impl/CompressFilePreviewImpl.java
@@ -3,10 +3,10 @@ package cn.keking.service.impl;
 import cn.keking.config.ConfigConstants;
 import cn.keking.model.FileAttribute;
 import cn.keking.model.ReturnResponse;
+import cn.keking.service.CompressFileReader;
+import cn.keking.service.FileHandlerService;
 import cn.keking.service.FilePreview;
 import cn.keking.utils.DownloadUtils;
-import cn.keking.service.FileHandlerService;
-import cn.keking.service.CompressFileReader;
 import cn.keking.utils.KkFileUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.poi.EncryptedDocumentException;
@@ -16,6 +16,7 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Created by kl on 2018/1/17.
@@ -39,6 +40,13 @@ public class CompressFilePreviewImpl implements FilePreview {
         String fileName=fileAttribute.getName();
         String filePassword = fileAttribute.getFilePassword();
         boolean forceUpdatedCache=fileAttribute.forceUpdatedCache();
+
+        String ConvertRecords = FileHandlerService.queryRecords(forceUpdatedCache,fileName,fileHandlerService);
+        if (Objects.equals(ConvertRecords, "error"))
+        { return otherFilePreview.notSupportedFile(model, fileAttribute, "文件["+fileName+"]转换失败，请联系系统管理员");
+        }else if (Objects.equals(ConvertRecords, "convert"))
+        { return otherFilePreview.notSupportedFile(model, fileAttribute, "文件["+fileName+"]正在转换中,请稍后刷新访问");
+        }
         String fileTree = null;
         // 判断文件名是否存在(redis缓存读取)
         if (forceUpdatedCache || !StringUtils.hasText(fileHandlerService.getConvertedFile(fileName))  || !ConfigConstants.isCacheEnabled()) {
@@ -48,12 +56,14 @@ public class CompressFilePreviewImpl implements FilePreview {
             }
             String filePath = response.getContent();
             try {
+                FileHandlerService.ConvertingMap.put(fileName, fileName);  //添加转换符号  //添加转换符号
                 fileTree = compressFileReader.unRar(filePath, filePassword,fileName);
             } catch (Exception e) {
                 Throwable[] throwableArray = ExceptionUtils.getThrowables(e);
                 for (Throwable throwable : throwableArray) {
                     if (throwable instanceof IOException || throwable instanceof EncryptedDocumentException) {
                         if (e.getMessage().toLowerCase().contains(Rar_PASSWORD_MSG)) {
+                            FileHandlerService.ConvertingMap.remove(fileName, fileName);  //加密文件删除缓存转换符号
                             model.addAttribute("needFilePassword", true);
                             return EXEL_FILE_PREVIEW_PAGE;
                         }
@@ -61,6 +71,7 @@ public class CompressFilePreviewImpl implements FilePreview {
                 }
             }
             if (!ObjectUtils.isEmpty(fileTree)) {
+                FileHandlerService.ConvertingMap.remove(fileName, fileName);  //转换成功删除缓存转换符号
                 //是否保留压缩包源文件
                 if (ConfigConstants.getDeleteSourceFile()) {
                     KkFileUtils.deleteFileByPath(filePath);
@@ -70,6 +81,7 @@ public class CompressFilePreviewImpl implements FilePreview {
                     fileHandlerService.addConvertedFile(fileName, fileTree);
                 }
             }else {
+                fileHandlerService.addConvertedFile(fileName, "error");  //转换错误加入缓存
                 return otherFilePreview.notSupportedFile(model, fileAttribute, "压缩文件密码错误! 压缩文件损坏!  压缩文件类型不受支持!");
             }
         } else {
--- server/src/main/java/cn/keking/service/impl/PdfFilePreviewImpl.java
+++ server/src/main/java/cn/keking/service/impl/PdfFilePreviewImpl.java
@@ -3,9 +3,9 @@ package cn.keking.service.impl;
 import cn.keking.config.ConfigConstants;
 import cn.keking.model.FileAttribute;
 import cn.keking.model.ReturnResponse;
+import cn.keking.service.FileHandlerService;
 import cn.keking.service.FilePreview;
 import cn.keking.utils.DownloadUtils;
-import cn.keking.service.FileHandlerService;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.poi.EncryptedDocumentException;
 import org.springframework.stereotype.Service;
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by kl on 2018/1/17.
@@ -39,6 +40,13 @@ public class PdfFilePreviewImpl implements FilePreview {
         boolean forceUpdatedCache=fileAttribute.forceUpdatedCache();
         String pdfName = fileName.substring(0, fileName.lastIndexOf(".") + 1) + "pdf";
         String outFilePath = FILE_DIR + pdfName;
+
+        String ConvertRecords = FileHandlerService.queryRecords(forceUpdatedCache,pdfName,fileHandlerService);
+        if (Objects.equals(ConvertRecords, "error"))
+        { return otherFilePreview.notSupportedFile(model, fileAttribute, "文件["+fileName+"]转换失败，请联系系统管理员");
+        }else if (Objects.equals(ConvertRecords, "convert"))
+        { return otherFilePreview.notSupportedFile(model, fileAttribute, "文件["+fileName+"]正在转换中,请稍后刷新访问");
+        }
         if (OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_IMAGE.equals(officePreviewType) || OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_ALL_IMAGES.equals(officePreviewType)) {
             //当文件不存在时，就去下载
             if (forceUpdatedCache || !fileHandlerService.listConvertedFiles().containsKey(pdfName) || !ConfigConstants.isCacheEnabled()) {
@@ -52,24 +60,27 @@ public class PdfFilePreviewImpl implements FilePreview {
                     fileHandlerService.addConvertedFile(pdfName, fileHandlerService.getRelativePath(outFilePath));
                 }
             }
-            List<String> imageUrls;
+            List<String> imageUrls = null;
             try {
+                FileHandlerService.ConvertingMap.put(pdfName, pdfName);  //添加转换符号  //添加转换符号
                 imageUrls = fileHandlerService.pdf2jpg(outFilePath, pdfName, fileAttribute);
             } catch (Exception e) {
                 Throwable[] throwableArray = ExceptionUtils.getThrowables(e);
                 for (Throwable throwable : throwableArray) {
                     if (throwable instanceof IOException || throwable instanceof EncryptedDocumentException) {
                         if (e.getMessage().toLowerCase().contains(PDF_PASSWORD_MSG)) {
+                            FileHandlerService.ConvertingMap.remove(pdfName, pdfName);  //加密文件删除缓存转换符号
                             model.addAttribute("needFilePassword", true);
                             return EXEL_FILE_PREVIEW_PAGE;
                         }
                     }
                 }
-                return otherFilePreview.notSupportedFile(model, fileAttribute, "pdf转图片异常，请联系管理员");
             }
             if (imageUrls == null || imageUrls.size() < 1) {
+                fileHandlerService.addConvertedFile(pdfName, "error");  //失败加入缓存
                 return otherFilePreview.notSupportedFile(model, fileAttribute, "pdf转图片异常，请联系管理员");
             }
+            FileHandlerService.ConvertingMap.remove(pdfName, pdfName);  //转换成功删除缓存转换符号
             model.addAttribute("imgUrls", imageUrls);
             model.addAttribute("currentUrl", imageUrls.get(0));
             if (OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_IMAGE.equals(officePreviewType)) {
